235395d7a0a8f532bb60d1333a558e89c128342c
獅子野成宏 <shix2@shix2-MacBook-Pro.local>
2022-04-10 12:03:09 +0900

    1st commit

---
+++ hello.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import time
+
+st.title('streamlit 超入門')
+
+st.write('プログレスバーの表示')
+'Start!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
+
+'Done!!!!!!!!'
+
+
+
+
+
+left_column, right_column = st.columns(2)
+button = left_column.button('右カラムに文字を表示')
+if button:
+    right_column.write('ここは右カラム')
+
+expander１ = st.expander('問い合わせ１')
+expander１.write('問い合わせ１の回答を書く')
+expander２ = st.expander('問い合わせ２')
+expander２.write('問い合わせ２の回答内容を書く')
+expander３ = st.expander('問い合わせ３')
+expander３.write('問い合わせ３の回答を書く')
